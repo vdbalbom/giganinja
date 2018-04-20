42c4e26979083a03ebbb84c40ccbbdb437928391
vdbalbom <virginiamonnerat@gmail.com>
2018-04-20 17:53:57 -0300

    prevent nil transportadora to pedido and nil fornecedor to produto when show their names

--- app/helpers/pedidos_helper.rb
+++ app/helpers/pedidos_helper.rb
@@ -1,2 +1,6 @@
 module PedidosHelper
+  def nome_transportadora pedido
+    return pedido.transportadora.name if pedido.transportadora
+    "none"
+  end
 end
--- app/helpers/produtos_helper.rb
+++ app/helpers/produtos_helper.rb
@@ -1,2 +1,6 @@
 module ProdutosHelper
+  def nome_fornecedor produto
+    return produto.fornecedor.nome if produto.fornecedor
+    "none"
+  end
 end
