8286b5f908708baa6e8c0c764dc6ebade9ee13ca
vdbalbom <virginiamonnerat@gmail.com>
2018-04-20 23:21:54 -0300

    validate numericality of valor > 0

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :pedido
   belongs_to :produto
   validates_numericality_of :quantidade, greater_than: 0
-  validates :valor, presence: true
+  validates_numericality_of :valor
 
   def subtotal
     self.quantidade*self.valor
